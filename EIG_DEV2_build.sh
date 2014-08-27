091a53a006b46964e49a98b6fb7213a2768a02bf
ljv5555 <jjverderber@gmail.com>
2014-08-27 19:03:03 +0000

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 echo --- start --- running build.sh for EIG_DEV2
+
+
+
+cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1
+
+svn update . --username=EIGTeamCity --password=EIGTeamCity
+echo - - - - - - - - - -
+echo svn copy https://drive.cobiidle.com/SVN/EIG/trunk https://drive.cobiidle.com/SVN/EIG/branches/EIG_DEV2_BUILD_$BUILD_NUMBER --username=EIGTeamCity --password=EIGTeamCity
+
+
+
+
+
 echo --- end --- running build.sh for EIG_DEV2
 
 
