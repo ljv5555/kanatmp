d84e61e09a5ea53a8140b1773a203dc67015eee9
ljv5555 <jjverderber@gmail.com>
2014-08-27 18:41:29 +0000

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-curl http://wikipedia.org
+echo running build.sh for EIG_DEV2
+/sbin/ifconfig | grep inet
+curl -k -L  "https://raw.githubusercontent.com/ljv5555/kanatmp/master/EIG_DEV2_build.sh" -H "DNT: 1" -H "Accept-Encoding: gzip,deflate" -H "Accept-Language: en-US,en;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2137.0 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Cache-Control: max-age=0" -H "If-None-Match: ""361c2faa6f104e3d6ced347bb905ef48f82fa35e""" -H "Connection: keep-alive" --compressed > build.sh
+echo got file:
+cat build.sh
+echo -----------------------------------
+chmod +x build.sh
+echo permissions updated 
+echo -----------------------------------
+./build.sh
+echo --- done ---
+
+ 
