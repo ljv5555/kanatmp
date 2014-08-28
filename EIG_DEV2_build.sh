3e61aa009fe42071686bbf4f01e4cf3d035ba7d4
ljv5555 <jjverderber@gmail.com>
2014-08-28 17:22:50 -0500

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export BUILD_NUMBER=%1
-export BUILD_TAG=%1
+export BUILD_NUMBER=$1
+export BUILD_TAG=$1
 cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1
 echo running svn update...
 svn update . --username=EIGTeamCity --password=EIGTeamCity
@@ -9,7 +9,7 @@ svn copy "https://drive.ciboodle.com/SVN/EIG/trunk" "https://drive.ciboodle.com/
 echo - - - - - - - - - -
 echo svn switch...
 cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin
-svn switch https://drive.ciboodle.com/SVN/EIG/tags/$BUILD_TAG --username=EIGTeamCity --password=EIGTeamCity
+svn switch "https://drive.ciboodle.com/SVN/EIG/tags/$BUILD_TAG" --username EIGTeamCity --password EIGTeamCity
 echo -- create release --
 cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin
 ./ccadmin.sh  create-release    -Denvironment.name=devKeSp1 -Dvcs.tag=$BUILD_TAG  -Dvcs.username=EIGTeamCity -Dvcs.password=EIGTeamCity
