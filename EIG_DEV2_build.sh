f97ba94036efb96a068e3b332372b13cba7302ab
ljv5555 <jjverderber@gmail.com>
2014-08-27 22:24:15 -0500

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -6,9 +6,15 @@ svn update . --username=EIGTeamCity --password=EIGTeamCity
 echo making build...
 echo svn copy https://drive.ciboodle.com/SVN/EIG/trunk https://drive.ciboodle.com/SVN/EIG/branches/${BUILD_TAG} --username=EIGTeamCity --password=EIGTeamCity
 svn copy https://drive.ciboodle.com/SVN/EIG/trunk https://drive.ciboodle.com/SVN/EIG/branches/${BUILD_TAG} -m "${BUILD_TAG} createdByJenkins-EIG_DEV2" --username=EIGTeamCity --password=EIGTeamCity
-
 echo - - - - - - - - - -
-
+echo -- create release --
+cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin
+./ccadmin.sh  create-release -Dvcs.tag=${BUILD_TAG}   -Denvironment.name=devKeSp1
+echo - - - - - - - - - -
+echo - - - - - - - - - -
+echo - - - - - - - - - -
+echo - - - - - - - - - -
+echo - - - - - - - - - -
 echo --- end --- running build.sh for EIG_DEV2 ${BUILD_TAG}
 
 
