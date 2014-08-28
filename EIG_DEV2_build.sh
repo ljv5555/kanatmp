0088dd2eb0bfa774711e8d70ed2f3017b8357573
ljv5555 <jjverderber@gmail.com>
2014-08-28 17:02:06 -0500

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-echo --- start --- running build.sh for EIG_DEV2
+export BUILD_NUMBER=%1
+export BUILD_TAG=%1
 cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1
 echo running svn update...
 svn update . --username=EIGTeamCity --password=EIGTeamCity
 echo making build...
-echo svn copy https://drive.ciboodle.com/SVN/EIG/trunk https://drive.ciboodle.com/SVN/EIG/branches/${BUILD_TAG} --username=EIGTeamCity --password=EIGTeamCity
-svn copy https://drive.ciboodle.com/SVN/EIG/trunk https://drive.ciboodle.com/SVN/EIG/branches/${BUILD_TAG} -m "${BUILD_TAG} createdByJenkins-EIG_DEV2" --username=EIGTeamCity --password=EIGTeamCity
+svn copy https://drive.ciboodle.com/SVN/EIG/trunk https://drive.ciboodle.com/SVN/EIG/tags/$BUILD_TAG -m "$BUILD_TAG createdByJenkins-EIG_DEV2" --username=EIGTeamCity --password=EIGTeamCity
 echo - - - - - - - - - -
+echo svn switch...
+cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin
+svn switch https://drive.ciboodle.com/SVN/EIG/tags/$BUILD_TAG --username=EIGTeamCity --password=EIGTeamCity
 echo -- create release --
 cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin
-export BUILD_TAG_0=`java -jar jseval.jar (new Date()).getTime()` 
-./ccadmin.sh  create-release -Dvcs.tag=$BUILD_TAG_0   -Denvironment.name=devKeSp1
+./ccadmin.sh  create-release    -Denvironment.name=devKeSp1 -Dvcs.tag=$BUILD_TAG  --username=EIGTeamCity --password=EIGTeamCity
 echo - - - - - - - - - -
 echo - - - - - - - - - -
 echo - - - - - - - - - -
 echo - - - - - - - - - -
 echo - - - - - - - - - -
-echo --- end --- running build.sh for EIG_DEV2 $BUILD_TAG_0
+echo --- end --- running build.sh for EIG_DEV2 $BUILD_TAG
 
 
 # while true; do ./myserver; echo restarting; done
