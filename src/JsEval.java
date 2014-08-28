3f8601427879908bac96397fed2a5cd5333304d2
ljv5555 <jjverderber@gmail.com>
2014-08-27 20:44:52 -0500

    .

--- src/JsEval.java
+++ src/JsEval.java
@@ -1,3 +1,8 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+
 
 public class JsEval{
 
@@ -5,8 +10,21 @@ public class JsEval{
   public JsEval(String[] args)
   {
     String rtn = " ";
+    //System.out.println(new java.util.Date()+"");
     for(String arg : args){rtn+=(arg+" ");}
-    
+    //System.out.print(rtn);
+    ScriptEngineManager sem = new ScriptEngineManager();
+    ScriptEngine se = sem.getEngineByName("JavaScript");
+    try {
+		System.out.print(String.valueOf(se.eval(rtn)));
+	} catch (ScriptException e) {
+		e.printStackTrace();
+	}
+  
+  }
+  public static void main(String[] args)
+  {
+	  JsEval jse = new JsEval(args);
   }
 
 
