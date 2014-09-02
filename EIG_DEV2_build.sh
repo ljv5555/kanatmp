ef4b3042ab3e0fa95e5d93873e2c576a9254014e
ljv5555 <jjverderber@gmail.com>
2014-09-02 10:03:32 -0500

    Update EIG_DEV2_build.sh

--- EIG_DEV2_build.sh
+++ EIG_DEV2_build.sh
@@ -43,20 +43,20 @@ echo ... ... ...
 
 
 ##
-#echo --- running cmd 2 ---
+echo --- running cmd 2 ---
 
-#export next_cmd="./ccadmin.sh create-release -lib lib/antlib -Ddefault.core.home=/home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1 -Dvcs.tag=$BUILD_TAG -Drelease.bundle.file=-Ddefault.core.home=/home/kana/KANAEnterprise/KE13R1/bundles/$BUILD_TAG.zip -Drelease.name=Resources.Releases.EIGRelease -Dsvn.username=EIGTeamCity -Dsvn.password=EIGTeamCity -Drepository.password=eT3qPmbu -Drepository.username=admin -verbose"
-#cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin/
-#echo "#!/bin/bash" > next_cmd.sh
-#echo $next_cmd >> next_cmd.sh
-#chmod +x next_cmd.sh
-#./next_cmd.sh
+export next_cmd="./ccadmin.sh create-release -lib lib/antlib -Ddefault.core.home=/home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1 -Dvcs.tag=$BUILD_TAG -Drelease.bundle.file=-Ddefault.core.home=/home/kana/KANAEnterprise/KE13R1/bundles/$BUILD_TAG.zip -Drelease.name=Resources.Releases.EIGRelease -Dsvn.username=EIGTeamCity -Dsvn.password=EIGTeamCity -Drepository.password=eT3qPmbu -Drepository.username=admin -verbose"
+cd /home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1/bin/
+echo "#!/bin/bash" > next_cmd.sh
+echo $next_cmd >> next_cmd.sh
+chmod +x next_cmd.sh
+echo `./next_cmd.sh`
 
 
 
 ######./ccadmin.sh create-tag -Denvironment.name=devKeSp1 -Dvcs.tag=jenkins-EIG_DEV2_Build-55 -Ddefault.core.home=/home/kana/KANAEnterprise/KE13R1/AgentDesktopSP1 -Dsvn.username=EIGTeamCity -Dsvn.password=EIGTeamCity -verbose
 
-#echo -- ran next_cmd.sh ---
+echo -- ran next_cmd.sh ---
 
 
 #./ccadmin.sh  create-tag    -Denvironment.name=devKeSp1 -Dvcs.tag=$BUILD_TAG -Ddefault.core.home=/home/kana/   -Dsvn.username=EIGTeamCity -Dsvn.password=EIGTeamCity -verbose
